13252dc0f0977e94098283c7caee18bebe1261f2
AlexLimon4ik <taa.alex777@gmail.com>
2025-03-22 10:06:57 +0100

    6

---
+++ Урок_2_2.py
@@ -0,0 +1,4 @@
+class Student():
+    def __init__(self):
+        self.height = 170
+    height = 160
---
+++ Уроки/Урок_2_1.py
@@ -0,0 +1,9 @@
+class Student():
+    height = 160
+    def __init__(self):
+        print(self.height)
+        self.height += 10
+        print(self.height)
+
+nick = Student()
+kate = Student()
