af9f183814947cab2f3b7654bd8b616e4b28b896
AlexLimon4ik <taa.alex777@gmail.com>
2025-04-05 10:37:50 +0200

    23

--- Уроки/Урок 6/Урок_6_6.py
+++ Уроки/Урок 6/Урок_6_6.py
@@ -4,4 +4,11 @@ warnings.simplefilter('ignore', SyntaxWarning)
 warnings.simplefilter('always', ImportWarning)
 
 warnings.warn("Warnings, no code here", SyntaxWarning)
-warnings.warn("Warnings, module not import", ImportWarning)
+
+
+try:
+    warnings.warn("Warnings, module not import", ImportWarning)
+except:
+    print("Warning processed")
+
+    {"name": 123,}
